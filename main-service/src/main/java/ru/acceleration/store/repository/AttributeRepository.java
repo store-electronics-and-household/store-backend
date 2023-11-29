d22542c0f353d47b576ab2d15b9b4b75df0080bb
Ovabor <ovabor1991@gmail.com>
2023-11-29 12:37:05 +0300

    roll back

--- main-service/src/main/java/ru/acceleration/store/repository/AttributeRepository.java
+++
@@ -1,8 +0,0 @@
-package ru.acceleration.store.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.acceleration.store.model.Attribute;
-
-public interface AttributeRepository extends JpaRepository<Attribute, Long> {
-
-}

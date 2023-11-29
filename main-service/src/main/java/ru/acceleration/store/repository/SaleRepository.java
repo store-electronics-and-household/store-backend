b0a3d5356f58ec1e69e5ce70eb66e72f32939006
Ovabor <ovabor1991@gmail.com>
2023-11-29 18:06:36 +0300

    add new entity classes

--- main-service/src/main/java/ru/acceleration/store/repository/SaleRepository.java
+++ main-service/src/main/java/ru/acceleration/store/repository/SaleRepository.java
@@ -3,7 +3,6 @@ package ru.acceleration.store.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-import ru.acceleration.store.model.Sale;
 
 import java.util.List;
 
